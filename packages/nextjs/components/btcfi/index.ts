2dae56744cf54734c8016c7d84110dab877753c9
Alejandro Betancourt <alebeta0000@gmail.com>
2025-12-28 17:12:14 -0500

    feat: add ProtocolStats component to display key lending metrics and create an index for BTCFi components

---
+++ packages/nextjs/components/btcfi/index.ts
@@ -0,0 +1,10 @@
+/**
+ * BTCFi Lending Components
+ * 
+ * Export all BTCFi-specific components for easy importing
+ */
+
+export { HealthFactorCard } from "./HealthFactorCard";
+export { DepositForm } from "./DepositForm";
+export { BorrowForm } from "./BorrowForm";
+export { ProtocolStats } from "./ProtocolStats";
