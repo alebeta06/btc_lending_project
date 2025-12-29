b0e2c5c11f53eee24e7f5a326fd160d4b9cece0f
Alejandro Betancourt <alebeta0000@gmail.com>
2025-12-29 00:31:16 -0500

    feat: Implement repay and withdraw functionality, and integrate real-time protocol statistics from the contract.

--- packages/nextjs/components/btcfi/index.ts
+++ packages/nextjs/components/btcfi/index.ts
@@ -9,4 +9,6 @@ export { SetOraclePriceForm } from "./SetOraclePriceForm";
 export { HealthFactorCard } from "./HealthFactorCard";
 export { DepositForm } from "./DepositForm";
 export { BorrowForm } from "./BorrowForm";
+export { RepayForm } from "./RepayForm";
+export { WithdrawForm } from "./WithdrawForm";
 export { ProtocolStats } from "./ProtocolStats";
