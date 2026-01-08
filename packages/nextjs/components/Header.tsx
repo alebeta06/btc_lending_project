649c9e80c35b36e298e286621784d76d377853cd
Alejandro Betancourt <alebeta0000@gmail.com>
2026-01-08 02:53:15 -0500

    logos in header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -149,18 +149,13 @@ export const Header = () => {
           passHref
           className="hidden lg:flex items-center gap-2 ml-4 mr-6 shrink-0"
         >
-          <div className="flex relative w-10 h-10">
-            <Image
-              alt="SE2 logo"
-              className="cursor-pointer"
-              fill
-              src="/logo.svg"
-            />
-          </div>
-          <div className="flex flex-col">
-            <span className="font-bold leading-tight">Scaffold-Stark</span>
-            <span className="text-xs">Starknet dev stack</span>
-          </div>
+          <Image
+            alt="BTCFi Lending Protocol"
+            width={220}
+            height={60}
+            src="/logo-horizontal.svg"
+            className="cursor-pointer"
+          />
         </Link>
         <ul className="hidden lg:flex lg:flex-nowrap menu menu-horizontal px-1 gap-2">
           <HeaderMenuLinks />
